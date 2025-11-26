dc5dc624bf2e56b5f83dce76e79b3697d2bcb976
htak0601 <taklee2@naver.com>
2025-11-26 09:26:44 +0900

    ✨Feat: font 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from "./providers";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { pretendard } from "./font";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,8 +14,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+    <html lang="ko">
+      <body className={`${pretendard.variable} antialiased`}>
         <Providers>{children}</Providers>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,25 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const page = () => (
-  <Tabs className="">
-    <TabsList>
-      <TabsTrigger value="account">Account</TabsTrigger>
-      <TabsTrigger value="password">Password</TabsTrigger>
-    </TabsList>
-    <TabsContent value="account">
-      <div className="">Account</div>
-    </TabsContent>
-    <TabsContent value="password">
-      <div className="">Password</div>
-    </TabsContent>
-  </Tabs>
+  <>
+    <Tabs className="">
+      <TabsList>
+        <TabsTrigger value="account">Account</TabsTrigger>
+        <TabsTrigger value="password">Password</TabsTrigger>
+      </TabsList>
+      <TabsContent value="account">
+        <div className="">Account</div>
+      </TabsContent>
+      <TabsContent value="password">
+        <div className="">Password</div>
+      </TabsContent>
+    </Tabs>
+
+    <div className="p-4">
+      <h1 className="text-3xl font-bold text-orange-500">안녕하세요</h1>
+      <p className="text-2xl font-semibold text-orange-800">본문 내용</p>
+    </div>
+  </>
 );
 
 export default page;
