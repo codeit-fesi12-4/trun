b6c9d94c81dd818be8f23c6181a8efc2d236e551
htak0601 <taklee2@naver.com>
2025-12-01 14:10:39 +0900

    ✨Feat: Mypage 관련 타입 정의 및 기본 UI 연결

---
+++ src/components/modules/mypage/type.ts
@@ -0,0 +1,38 @@
+export type TProfileCardProps = {
+  teamId?: number;
+  id?: number;
+  email: string;
+  name: string;
+  companyName: string;
+  image: string;
+};
+
+export type Status = "이용 예정" | "개설 확정" | "개설 대기" | "이용 완료";
+export type TMyPageCardProps = {
+  teamId: number;
+  id: number;
+  type: "DALLAEMFIT" | "OFFICE_STRETCHING" | "MINDFULNESS" | "WORKATION";
+  name: string;
+  dateTime: string;
+  registrationEnd: string;
+  location: string;
+  participantCount: number;
+  capacity: number;
+  image: string;
+  status?: Status | Status[];
+  createdBy: number;
+  canceledAt: string | null;
+  joinedAt?: string;
+  isCompleted?: boolean;
+  isReviewed?: boolean;
+};
+
+export type TReviewCardProps = {
+  id: number;
+  image: string;
+  name: string;
+  location: string;
+  dateTime: string;
+  score?: number;
+  comment?: string;
+};
