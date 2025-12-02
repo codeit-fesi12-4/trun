781e87cd02d8718a65790d6e5ef844bf3b7a88fa
htak0601 <taklee2@naver.com>
2025-12-02 15:56:58 +0900

    💄Design: 글로벌 Header UI 디자인 시안2 변경

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -4,14 +4,19 @@ import Image from "next/image";
 import Link from "next/link";
 
 const Header = () => (
-  <header className="fixed top-0 z-10 w-full border-b-2 border-gray-900 bg-orange-600">
-    <div className="mx-auto flex h-14 max-w-[1200px] items-center justify-between px-4 md:h-15 md:px-6">
-      <div className="flex items-center gap-3 md:gap-6">
+  <header className="bg-background fixed top-0 z-10 w-full px-4 shadow-xl md:px-6">
+    <div className="mx-auto flex h-[50px] max-w-[1200px] items-center justify-between sm:h-[88px]">
+      <div className="flex items-center justify-center gap-4 md:gap-9">
         <Link href="/">
-          <Image src="/icons/logo.svg" alt="trun 로고" width={60} height={60} />
+          <Image
+            src="/icons/ic_logo.svg"
+            alt="trun 로고"
+            width={112}
+            height={112}
+            className="w-18 sm:w-28"
+          />
         </Link>
-
-        <nav className="flex gap-3 md:gap-6">
+        <nav className="flex gap-4 md:gap-9">
           <Link href="/moim-find" className="nav-link">
             모임 찾기
           </Link>
