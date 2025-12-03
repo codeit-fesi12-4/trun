e8f221b9ccb538b04c52e65ddd6092b2226d63b3
mintaek <omt610@gmail.com>
2025-12-03 11:23:51 +0900

    💄Design: 필터 컴포넌트 반응형 레이아웃 조정

--- src/components/modules/moim-find/MoimFindCategory.tsx
+++ src/components/modules/moim-find/MoimFindCategory.tsx
@@ -12,13 +12,13 @@ const MoimFindCategory = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
-    <div className="flex h-[95px] flex-col sm:h-[53px] sm:flex-row sm:items-center sm:justify-between sm:border-b sm:border-gray-200">
+    <div className="flex h-[95px] flex-col md:h-[53px] md:flex-row md:items-center md:justify-between md:border-b md:border-gray-200">
       <Tabs defaultValue="달림핏">
         <TabsList className="flex w-full justify-between bg-transparent">
-          <div className="flex w-full border-b border-gray-200 sm:border-0">
+          <div className="flex w-full border-b border-gray-200 md:border-0">
             <TabsTrigger
               value="달림핏"
-              className="h-[53px] w-1/2 gap-2 bg-transparent! px-6 text-base font-semibold text-gray-500 data-[state=active]:rounded-none data-[state=active]:border-x-0 data-[state=active]:border-t-0 data-[state=active]:border-b-2 data-[state=active]:border-green-500 data-[state=active]:text-green-600 data-[state=active]:shadow-none sm:text-xl"
+              className="h-[53px] w-1/2 gap-2 bg-transparent! px-6 text-base font-semibold text-gray-500 data-[state=active]:rounded-none data-[state=active]:border-x-0 data-[state=active]:border-t-0 data-[state=active]:border-b-2 data-[state=active]:border-green-500 data-[state=active]:text-green-600 data-[state=active]:shadow-none md:text-xl"
             >
               <Image
                 src="../icons/dallimfit.svg"
@@ -31,21 +31,21 @@ const MoimFindCategory = () => {
             </TabsTrigger>
             <TabsTrigger
               value="런케이션"
-              className="h-[53px] w-1/2 gap-2 bg-transparent! px-6 text-base font-semibold text-gray-500 data-[state=active]:rounded-none data-[state=active]:border-x-0 data-[state=active]:border-t-0 data-[state=active]:border-b-2 data-[state=active]:border-green-500 data-[state=active]:text-green-600 data-[state=active]:shadow-none sm:text-xl"
+              className="h-[53px] w-1/2 gap-2 bg-transparent! px-6 text-base font-semibold text-gray-500 data-[state=active]:rounded-none data-[state=active]:border-x-0 data-[state=active]:border-t-0 data-[state=active]:border-b-2 data-[state=active]:border-green-500 data-[state=active]:text-green-600 data-[state=active]:shadow-none md:text-xl"
             >
               <Image
                 src="../icons/runcation.svg"
                 alt="런케이션 아이콘"
                 width={40}
                 height={40}
-                className="sm:size-13"
+                className="md:size-13"
               />
               런케이션
             </TabsTrigger>
           </div>
         </TabsList>
       </Tabs>
-      <div className="mt-8 flex flex-row sm:mt-0 sm:px-3">
+      <div className="mt-8 flex flex-row md:mt-0 md:px-3">
         <MoimFindLocationFilter />
         <MoimFindDatePicker />
         <MoimFindSort />
