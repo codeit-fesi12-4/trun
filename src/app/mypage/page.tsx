e9311670909bf792194e27d55439aa7a91921fda
htak0601 <taklee2@naver.com>
2025-12-04 19:50:48 +0900

    ✨Feat: 프로필 수정 모달 open 기능 추가

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,17 +1,19 @@
 "use client";
 
+import ProfileEditModal from "@/components/modules/mypage/mypage-modal/ProfileEditModal";
 import ProfileSection from "@/components/modules/mypage/ProfileCard";
 import TabsSection from "@/components/modules/mypage/TabsSection";
 import { useAuthStore } from "@/stores/auth.store";
 import Image from "next/image";
-
-// 모달 테스트
-const onEditHandler = () => {
-  alert("프로필 수정 모달");
-};
+import { useState } from "react";
 
 const Mypage = () => {
   const user = useAuthStore(state => state.user);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleModalOpen = () => {
+    setIsModalOpen(true);
+  };
 
   return (
     <main className="flex w-full flex-1 flex-col lg:flex-row">
@@ -21,7 +23,7 @@ const Mypage = () => {
           <h1 className="text-base font-semibold text-gray-900 sm:text-2xl">마이페이지</h1>
 
           <button
-            onClick={onEditHandler}
+            onClick={handleModalOpen}
             className="cursor-pointer lg:absolute lg:top-[68px] lg:right-[52px]"
           >
             <Image src="/icons/ic_mypage_edit.svg" alt="수정 아이콘" width={32} height={32} />
@@ -46,6 +48,9 @@ const Mypage = () => {
       <div className="w-full">
         <TabsSection />
       </div>
+
+      {/* 프로필 수정 모달 */}
+      <ProfileEditModal open={isModalOpen} onOpenChange={setIsModalOpen} />
     </main>
   );
 };
