517c286ba986cddc6a4666ed9279262c16d3ef5a
mintaek <omt610@gmail.com>
2025-12-05 11:17:48 +0900

    💄Design: 찜한 모인 페이지 UI 구현 (기존 모임 찾기 페이지 컴포넌트 재사용)

--- src/app/moim-favorite/page.tsx
+++ src/app/moim-favorite/page.tsx
@@ -1,9 +1,28 @@
-import MoimFavoriteHeader from "@/components/modules/moim-favorite/MoimFavoriteHeader";
+"use client";
 
-const MoimFavoritePage = () => (
-  <div>
-    <MoimFavoriteHeader />
-  </div>
-);
+import MoimFavoriteHeader from "@/components/modules/moim-favorite/MoimFavoriteHeader";
+import MoimCardList from "@/components/modules/moim-find/MoimCardList";
+import { useMoimFind } from "@/hooks/useMoimFind";
+
+const MoimFavoritePage = () => {
+  const { moimCardData, isLoading, error, handleFilterChange, availableLocations } = useMoimFind();
+  return (
+    <>
+      <MoimFavoriteHeader
+        onFilterChange={handleFilterChange}
+        availableLocations={availableLocations}
+      />
+      {isLoading && (
+        <div className="mt-6 text-center text-gray-500">모임 목록을 불러오는 중...</div>
+      )}
+      {error && (
+        <div className="mt-6 text-center text-red-500">
+          모임 목록을 불러오는데 실패했습니다. 다시 시도해주세요.
+        </div>
+      )}
+      {!isLoading && !error && <MoimCardList items={moimCardData} />}
+    </>
+  );
+};
 
 export default MoimFavoritePage;
