b1dcc330f06df4a980bff02531aeec0b7e8edcc5
htak0601 <taklee2@naver.com>
2025-12-12 17:39:33 +0900

    ✨Feat: 리뷰 추가 API

--- src/api/review.api.ts
+++ src/api/review.api.ts
@@ -1,5 +1,5 @@
 import { API_BASE_URL, TEAM_NAME } from "@/constants";
-import { GetReviewsParams, ReviewScoresParams } from "@/types/review.type";
+import { GetReviewsParams, PostReviewParams, ReviewScoresParams } from "@/types/review.type";
 import { toast } from "sonner";
 
 // 모든 리뷰 Path
@@ -115,5 +115,41 @@ export const getReviewScores = async (params: ReviewScoresParams) => {
   } catch (error) {
     console.error("데이터 가져오기 오류:", error);
     return null;
+  }
+};
+
+// 리뷰 추가
+export const postReviews = async (
+  { gatheringId, score, comment }: PostReviewParams,
+  token: string,
+) => {
+  if (!token) {
+    throw new Error("로그인이 필요합니다.");
+  }
+
+  try {
+    const response = await fetch(`${API_BASE_URL}${TEAM_NAME}/reviews`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        gatheringId,
+        score,
+        comment,
+      }),
+    });
+
+    const result = await response.json();
+    if (!response.ok) {
+      throw new Error(result.message ?? "리뷰 작성에 실패했습니다.");
+    }
+
+    return result;
+  } catch (error) {
+    console.error("리뷰 작성 오류:", error);
+    throw error;
   }
 };
