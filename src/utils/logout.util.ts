e455ebcee15358ef0e6f2240f71449026e5269d8
Boyoung Lee <osoon9295@gmail.com>
2025-12-19 10:37:34 +0900

    🗑️Remove: apiClient2(초기 api client) 삭제, logout에서 login으로 넘겨주는 코드 제거

--- src/utils/logout.util.ts
+++ src/utils/logout.util.ts
@@ -4,11 +4,6 @@ import { toast } from "sonner";
 export const logout = async () => {
   try {
     await signOut({ redirect: false });
-    // toast.error("로그인 기간이 만료되었습니다. 다시 로그인 해주세요.");
-
-    if (typeof window !== "undefined") {
-      window.location.href = "/login";
-    }
   } catch (error) {
     console.error("Logout error:", error);
     toast.error("로그아웃 중 오류가 발생했습니다.");
