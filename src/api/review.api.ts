3a950f5aeee2722094a4498b4bab0a47696f104c
Boyoung Lee <osoon9295@gmail.com>
2025-12-22 11:35:58 +0900

    🐛Fix: 리뷰 요청 url변경 => 308 해결

--- src/api/review.api.ts
+++ src/api/review.api.ts
@@ -12,19 +12,19 @@ import { buildReviewScoresPath, buildReviewsPath } from "@/utils/path.util";
 
 // 모든 리뷰 가져오기
 export const getReviews = (params: GetReviewsParams) =>
-  apiFetch<GetReviewsResponse>(`/api/proxy/${buildReviewsPath(params)}`, {
+  apiFetch<GetReviewsResponse>(`/api/proxy${buildReviewsPath(params)}`, {
     method: "GET",
   });
 
 // 모임별 리뷰 가져오기
 export const getMoimReviews = (params: GetReviewsParams) =>
-  apiFetch<GetMoimReviewsResponse>(`/api/proxy/${buildReviewsPath(params)}`, {
+  apiFetch<GetMoimReviewsResponse>(`/api/proxy${buildReviewsPath(params)}`, {
     method: "GET",
   });
 
 // 리뷰 평점 가져오기
 export const getReviewScores = (params: GetReviewScoresParams) =>
-  apiFetch<GetReviewScoresResponse>(`/api/proxy/${buildReviewScoresPath(params)}`, {
+  apiFetch<GetReviewScoresResponse>(`/api/proxy${buildReviewScoresPath(params)}`, {
     method: "GET",
   });
 
