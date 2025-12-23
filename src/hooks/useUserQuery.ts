d7b881fd47c52f7a47ead614d0503b31b24c5072
htak0601 <taklee2@naver.com>
2025-12-23 15:58:34 +0900

    ♻️Refactor: 불필요한 코드 제거

--- src/hooks/useUserQuery.ts
+++ src/hooks/useUserQuery.ts
@@ -1,20 +1,16 @@
 import { putUpdateProfile } from "@/api/user.api";
-import { UserProfile } from "@/types/user.type";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 import { toast } from "sonner";
 
 // 회원 정보 수정
-export const useUpdateProfileMutationQuery = () => {
-  const queryClient = useQueryClient();
-
-  return useMutation({
+export const useUpdateProfileMutationQuery = () =>
+  useMutation({
     mutationFn: (formData: FormData) => putUpdateProfile(formData),
     onSuccess: res => {
       if (!res.ok) {
         toast(res.message);
         return;
       }
-      queryClient.setQueryData<UserProfile>(["userProfile"], res.data);
       toast("프로필이 수정되었습니다");
     },
     onError: error => {
@@ -22,4 +18,3 @@ export const useUpdateProfileMutationQuery = () => {
       else toast("프로필 업데이트 중 오류가 발생했습니다");
     },
   });
-};
