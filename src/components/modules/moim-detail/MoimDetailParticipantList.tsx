55985624cee1611806230dc33d2f975505f4c80f
Boyoung Lee <osoon9295@gmail.com>
2025-12-23 11:44:43 +0900

    🐛Fix: 리뷰 참가자 이미지를 특정 동그란 원 안에 가득차 보이게 수정

--- src/components/modules/moim-detail/MoimDetailParticipantList.tsx
+++ src/components/modules/moim-detail/MoimDetailParticipantList.tsx
@@ -15,18 +15,18 @@ const MoimDetailParticipantList = ({ participants }: MoimDetailParticipantListPr
       <ul className="relative flex">
         {visibleParticipants.map((p, idx) => (
           <li key={p.userId} className={idx === 0 ? "" : "-ml-2.5"}>
-            <div className="h-[29px] w-[29px] overflow-hidden rounded-full">
+            <div className="relative h-[29px] w-[29px] overflow-hidden rounded-full">
               <Image
                 src={p.User.image ? p.User.image : "../icons/default_profile.svg"}
                 alt={`${p.User.name}의 프로필사진`}
-                width={29}
-                height={29}
+                fill
+                className="object-cover"
               />
             </div>
           </li>
         ))}
         {isTooMany && (
-          <div className="-ml-2.5 flex h-[29px] w-[29px] items-center justify-center rounded-full bg-white text-xs font-semibold text-gray-600">
+          <div className="z-10 -ml-2.5 flex h-[29px] w-[29px] items-center justify-center rounded-full bg-white text-xs font-semibold text-gray-600">
             +{participants.length - 4}
           </div>
         )}
