a8102e4a2a9d5f20bb1728f584818b26e1fb412a
htak0601 <taklee2@naver.com>
2026-01-02 16:31:27 +0900

    🔧Chore: 불필요한 buildAuthPath 제거

--- src/api/auth.api.ts
+++ src/api/auth.api.ts
@@ -2,11 +2,9 @@ import { API_BASE_URL, TEAM_NAME } from "@/constants/env";
 import { apiFetch } from "@/lib/apiClient";
 import { SigninRequest, SignupRequest, LoginResponse, SignupResponse } from "@/types/auth.type";
 
-const buildAuthPath = (path: string, teamName: string) => `${API_BASE_URL}${teamName}/auths${path}`;
-
 // 회원가입
-export const postSignup = (payload: SignupRequest, teamName: string = TEAM_NAME) =>
-  apiFetch<SignupResponse & { message: string }>(buildAuthPath("/signup", teamName), {
+export const postSignup = (payload: SignupRequest) =>
+  apiFetch<SignupResponse & { message: string }>(`/api/proxy/auths/signup`, {
     method: "POST",
     headers: {
       Accept: "application/json",
@@ -15,8 +13,8 @@ export const postSignup = (payload: SignupRequest, teamName: string = TEAM_NAME)
   });
 
 // 로그인
-export const postSignin = (payload: SigninRequest, teamName: string = TEAM_NAME) =>
-  apiFetch<LoginResponse>(buildAuthPath("/signin", teamName), {
+export const postSignin = (payload: SigninRequest) =>
+  apiFetch<LoginResponse>(`${API_BASE_URL}${TEAM_NAME}/auths/signin`, {
     method: "POST",
     headers: {
       Accept: "application/json",
